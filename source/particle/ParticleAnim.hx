f2edddc7b1d1fca36a7f8815097fbc88cfff08f9
Omaraldin <47356302+Omaraldin@users.noreply.github.com>
2020-06-05 22:21:58 +0200

    Improved ParticleAnim

--- source/particle/ParticleAnim.hx
+++ source/particle/ParticleAnim.hx
@@ -1,19 +1,21 @@
 package particle;
 
+import openfl.events.Event;
 import flash.display.BitmapData;
 import openfl.display.Bitmap;
 
 class ParticleAnim extends Bitmap {
 	public var zero: ParticleZero;
 	public var currentImage: ParticleImage;
-	public var currentIndex: Int = 0;
-	public var stopIndex: Int = 0;
+	public var currentImageIndex: Int = 0;
 	public var repeat: Bool = false;
+	public var rendered: Bool = false;
+	public var stopped: Bool = false;
 	public var startFrame: Int = 0;
-	public var endFrame: Int = 0;
-	
+
+
 	public function new(zero: ParticleZero, repeat: Bool = false, w: Int = 0, h: Int = 0, x: Float = 0, y: Float = 0) {
-		super();
+		super(null, 'auto', true);
 		this.zero = zero;
 		if (x == 0)
 			this.x = -(this.width / 2);
@@ -21,35 +23,56 @@ class ParticleAnim extends Bitmap {
 			this.y = -(this.height / 1.5);
 		this.repeat = repeat;
 		this.bitmapData = new BitmapData(w, h, true, 0);
+		Transformice.instance.addEventListener(Event.ENTER_FRAME, this.onEnterFrame);
 	}
 
-	public function setStartStop(start: Int = -1, stop: Int = -1) {
-		if (start != -1)
-			this.startFrame = start;
-		if (stop != -1)
-			this.endFrame = stop;
-		this.currentIndex = this.startFrame;
-		this.stopIndex = this.endFrame;
+	private function onEnterFrame(event: Event) {
+		if (this.rendered) {
+			if (this.currentImageIndex >= this.zero.totalFrames) {
+				if (this.repeat) 
+					this.currentImageIndex = this.startFrame;
+				else
+					Transformice.instance.removeEventListener(Event.ENTER_FRAME, this.onEnterFrame);
+
+			} else 
+				this.render();
+		}
+	}
+
+	public function gotoAndStop(frame: Int) {
+		if (frame < this.zero.totalFrames) {
+			Transformice.instance.removeEventListener(Event.ENTER_FRAME, this.onEnterFrame);
+			this.repeat = false;
+			this.stopped = true;
+			this.currentImageIndex = frame;
+			this.currentImage = this.zero.listImages[this.currentImageIndex];
+			this.currentImage.render();
+			this.bitmapData = this.currentImage.bitmapData;
+			this.x = this.currentImage.x;
+			this.y = this.currentImage.y;
+		}
+	}
+
+	public function gotoAndPlay(frame: Int) {
+		if (frame < this.zero.totalFrames) {
+			this.startFrame = frame;
+			this.currentImageIndex = frame;
+			this.render();
+		}
 	}
 
 	public function render(): Void {
-		if (this.currentIndex == this.stopIndex)
-			this.setStartStop(0, this.zero.totalFrames);
-		if (this.currentIndex >= this.stopIndex) {
-			if (this.repeat) {
-				this.currentIndex = this.startFrame;
-				this.stopIndex = this.endFrame;
-			} else {};
-		}
-		this.currentImage = this.zero.listImages[this.currentIndex];
+		if (this.stopped) return;
+		if (!this.rendered) this.rendered = true;
+		this.currentImage = this.zero.listImages[this.currentImageIndex];
 		if (this.currentImage == null) {
-			this.currentIndex++;
+			this.currentImageIndex++;
 			return;
-		};
+		}
 		this.currentImage.render();
 		this.bitmapData = this.currentImage.bitmapData;
 		this.x = this.currentImage.x;
 		this.y = this.currentImage.y;
-		this.currentIndex++;
+		this.currentImageIndex++;
 	}
 }
